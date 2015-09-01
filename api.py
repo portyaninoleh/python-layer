c4d98a7697b52a5c6f76f98daf2323819c83ffad
Odesk Odesk <odeskcompanyunit@gmail.com>
2015-09-01 18:29:08 +0300

    Completed the send messages and send Announcements functionality

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import requests
 import json
 import uuid
 import datetime
+import copy
 
 from constants import BASE_URL
 from utils import set_metadata
@@ -25,6 +26,141 @@ class LayerBase(object):
             'Content-Type': 'application/json',
             'If-None-Match': unicode(uuid.uuid4())
         }
+
+
+class MessageBase(object):
+    """
+    Base structure for messages and announcements
+    """
+    def __init__(self, parts,
+                 sender,
+                 conversation,
+                 message_id=None,
+                 notification=None,
+                 url=None,
+                 sent_at=None,
+                 recipient_status=None):
+        """
+        Constructor
+        :param parts: list() - list of MessageParts objects
+        :param sender: list() - list of Sender objects
+        :param conversation: Conversation() - conversation object
+        :param message_id: unicode() - message id
+        :param notification: Notification() - notification object
+        :param url: unicode() - message url
+        :param sent_at: datetime() - sent at date
+        :param recipient_status: dict() - list of recipients and message status for them
+        :return: No results returned
+        """
+        self.parts = parts
+        self.notification = notification
+        self.sender = sender
+        self.conversation = conversation
+        self.message_id = message_id
+        self.url = url
+        self.sent_at = sent_at
+        self.recipient_status = recipient_status
+
+
+class Notification(object):
+    """
+    Class for handling notifications
+    """
+    def __init__(self, text, recipients, sound='chime.aiff'):
+        """
+        Constructor
+        :param text: unicode() - notification body
+        :param recipients: list() - list of recipients
+        :param sound: unicode() - sound name
+        :return:
+        """
+        self.text = text
+        self.sound = sound
+        self.recipients = recipients
+
+    def get_entity(self):
+        result = {'text': self.text,
+                  'sound': self.sound}
+        if self.recipients:
+            result['recipients'] = self.recipients
+
+
+class Sender(object):
+    def __init__(self, name=None, sender_id=None):
+        self.name = name
+        self.sender_id = sender_id
+
+    def __unicode__(self):
+        return self.sender_id if self.sender_id else self.name
+
+    def get_entity(self):
+        if self.sender_id:
+            return {'user_id': self.sender_id}
+        return {'name': self.name}
+
+
+class MessageParts(object):
+    def __init__(self, body, mime_type, encoding=None):
+        self.body = body
+        self.mime_type = mime_type
+        self.encoding = encoding
+
+    def get_part(self):
+        result = {'body': self.body,
+                  'mime_type': self.mime_type}
+        if self.encoding:
+            result['encoding'] = self.encoding
+        return result
+
+
+class Announcements(MessageBase):
+
+    def __init__(self, parts, sender, conversation):
+        super(Announcements, self).__init__(parts=parts, sender=sender, conversation=conversation)
+        self.recipients = list()
+
+    def send(self, recipients=None, everyone=False):
+        data = {'sender': self.sender.get_entity(),
+                'parts': [i.get_part() for i in self.parts]}
+        if everyone:
+            data['recipients'] = 'everyone'
+        else:
+            data['recipients'] = recipients
+        result = requests.post('{}/announcements'.format(self.conversation.url),
+                               data=json.dumps(data),
+                               headers=self.conversation.origin_headers)
+        if result.status_code == 202:
+            r = json.loads(result.text)
+            self.message_id = r['id']
+            self.url = r['url']
+            self.sent_at = datetime.datetime.strptime(r['sent_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
+            self.recipients = r['recipients']
+            return self
+        else:
+            raise Exception(result.text)
+
+
+class Message(MessageBase):
+
+    def send(self):
+        data = {'sender': self.sender.get_entity(),
+                'parts': [i.get_part() for i in self.parts]}
+        # TODO: Left for future
+        # if self.notification:
+        #     data['notification'] = self.notification
+        result = requests.post('{}/conversations/{}/messages'.format(self.conversation.url,
+                                                                     self.conversation.conversation_id),
+                               data=json.dumps(data),
+                               headers=self.conversation.origin_headers)
+        if result.status_code == 201:
+            r = json.loads(result.text)
+            self.message_id = r['id']
+            self.url = r['url']
+            self.sent_at = datetime.datetime.strptime(r['sent_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
+            self.recipient_status = r['recipient_status']
+            return self
+        else:
+            raise Exception(result.text)
 
 
 class Conversation(LayerBase):
@@ -59,6 +195,7 @@ class Conversation(LayerBase):
         self.distinct = distinct
         self.metadata = metadata
         self.created_at = datetime.datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%fZ')
+        self.origin_headers = copy.copy(self.headers)
         self.headers['Content-Type'] = 'application/vnd.layer-patch+json'
 
     def add_participants(self, participants):
